b61262d07eb056c97f4725de7ee8a2718a325baa
SuperMiguelo <mi.leruyet@hotmail.fr>
2022-04-20 15:53:49 +0200

    updt 2

---
+++ list.js
@@ -0,0 +1,6 @@
+                    function getValue() {
+                    var x = document.createElement("li");
+                    var t = document.getElementById("in");
+                    x.appendChild(t);
+                    document.getElementById("myList").appendChild(x);
+                    }
