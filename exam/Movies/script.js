a4905671067007d492819201eae357b3fd4aa098
SuperMiguelo <mi.leruyet@hotmail.fr>
2022-04-22 16:20:42 +0200

    updt movies

--- exam/Movies/script.js
+++ exam/Movies/script.js
@@ -1,40 +1,22 @@
-window.addEventListener('DOMContentLoaded', () => {
-    
-    document.getElementById("rechercher").addEventListener("click", () => {
-        fetchMovie();
-    });
-});
+
 
 function fetchMovie(apiKey = '3f951fde1f94ff23e3aebbd24b292474') {
-    let search =  document.getElementById('search').value || 'default';
-    let limit =  document.getElementById('limit').value || 10;
+    
     let resultSearch = document.getElementById('resultSearch');
 
-    fetch(`https://g.tenor.com/v1/search?q=${search}&key=${apiKey}&limit=${limit}`)
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&langua
+    ge=fr-FR&page=1`)
         .then(response => response.json())
         .then(movies => {
             console.log(movies);
             movies.results.forEach(movie => {
-                const image = document.createElement('div');
-                image.src = `${movie.itemurl}.movie`;
+                const image = document.createElement('img');
+                console.log(movie);
+                image.src = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/"+movie.poster_path
+                
                 resultSearch.appendChild(image);
             });
         })
         .catch(err => console.log(err));
 }
 
-async function fetchMovieAsync(apiKey = '3f951fde1f94ff23e3aebbd24b292474') {
-    let search =  document.getElementById('search').value || 'default';
-    let limit =  document.getElementById('limit').value || 10;
-    let resultSearch = document.getElementById('resultSearch');
-
-    const movies = await fetch(`https://g.tenor.com/v1/search?q=${search}&key=${apiKey}&limit=${limit}`)
-        .then(response => response.json())
-        .then(movies => movies.results);
-
-    movies.forEach(movie => {
-        const image = document.createElement('div');
-        image.src = `${movie.itemurl}.movie`;
-        resultSearch.appendChild(image);
-    });
-}
